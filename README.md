6b04c0ebea8be0cc534af77e63f7a6321f82e070
toXel <timo625@gmail.com>
2017-02-01 21:32:15 +0100

    :construction: Implement first csv export

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 This will (hopefully) be a little tool to export passwords from [pass](http://passwordstore.org) to other password managers.
 
-At the moment this decrypts all your pass entries and prints them to stdout so use it with caution!
+At the moment this decrypts all your pass entries and writes them into a plan CSV file so use it with caution!
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from pypass import PasswordStore
+import csv
+from pypass import PasswordStore, EntryType
 
 
 def main():
     store = PasswordStore()
     passwords = store.get_passwords_list()
-    for pw in passwords:
-        print(store.get_decypted_password(pw))
+    with open('pass.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for pw in passwords:
+            csvwriter.writerow([store.get_decypted_password(pw, EntryType.password), store.get_decypted_password(pw)])
 
 
 if __name__ == '__main__':
